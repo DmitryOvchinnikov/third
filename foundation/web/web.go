4fe55da49ae6de614191b697989608138a6f527a
Dmitry Ovchinnikov <dmitry.v.ovchinnikov@gmail.com>
2023-01-06 15:00:28 +0300

    6.3 Middleware Support

--- foundation/web/web.go
+++ foundation/web/web.go
@@ -20,16 +20,18 @@ type Handler func(ctx context.Context, w http.ResponseWriter, r *http.Request) e
 type App struct {
 	mux      *httptreemux.ContextMux
 	shutdown chan os.Signal
+	mw       []Middleware
 }
 
 // NewApp creates an App value that handle a set of routes for the application.
-func NewApp(shutdown chan os.Signal) *App {
+func NewApp(shutdown chan os.Signal, mw ...Middleware) *App {
 
 	mux := httptreemux.NewContextMux()
 
 	return &App{
 		mux,
 		shutdown,
+		mw,
 	}
 }
 
@@ -46,7 +48,13 @@ func (a *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 // Handle sets a handler function for a given HTTP method and path pair
 // to the application server mux.
-func (a *App) Handle(method string, group string, path string, handler Handler) {
+func (a *App) Handle(method string, group string, path string, handler Handler, mw ...Middleware) {
+
+	// First wrap handler specific middleware around this handler.
+	handler = wrapMiddleware(mw, handler)
+
+	// Add the application's general middleware to the handler chain.
+	handler = wrapMiddleware(a.mw, handler)
 
 	// The function to execute for each request.
 	h := func(w http.ResponseWriter, r *http.Request) {
