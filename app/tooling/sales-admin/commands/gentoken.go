8cc09343b8768e6de27916af5cc1b4882b2e32c1
Dmitry Ovchinnikov <dmitry.v.ovchinnikov@gmail.com>
2023-01-07 18:22:51 +0300

    8.4 Token Signature Validation

--- app/tooling/sales-admin/commands/gentoken.go
+++ app/tooling/sales-admin/commands/gentoken.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"crypto/x509"
 	"encoding/pem"
+	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -96,6 +97,45 @@ func GenToken() error {
 	if err := pem.Encode(os.Stdout, &publicBlock); err != nil {
 		return fmt.Errorf("encoding to public file: %w", err)
 	}
+
+	/*
+		Token validation
+	*/
+
+	// Create the token parser to use. The algorithm used to sign the JWT must be
+	// validated to avoid a critical vulnerability:
+	// https://auth0.com/blog/critical-vulnerabilities-in-json-web-token-libraries/
+	parser := jwt.NewParser(jwt.WithValidMethods([]string{"RS256"}))
+
+	var parsedClaims struct {
+		jwt.RegisteredClaims
+		Roles []string
+	}
+
+	keyFunc := func(t *jwt.Token) (interface{}, error) {
+		kid, ok := t.Header["kid"]
+		if !ok {
+			return nil, errors.New("missing key id (kid) in token header")
+		}
+		kidID, ok := kid.(string)
+		if !ok {
+			return nil, errors.New("user token key id (kid) must be string")
+		}
+		fmt.Println("KID:", kidID)
+
+		return &privateKey.PublicKey, nil
+	}
+
+	parsedToken, err := parser.ParseWithClaims(tokenStr, &parsedClaims, keyFunc)
+	if err != nil {
+		return fmt.Errorf("parsing token: %w", err)
+	}
+
+	if !parsedToken.Valid {
+		return errors.New("invalid token")
+	}
+
+	fmt.Println("-----VALID TOKEN-----")
 
 	return nil
 }
